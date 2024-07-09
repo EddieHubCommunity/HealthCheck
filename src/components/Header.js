4139416857fc6facb53723c25d94af5ef2e8f406
Eddie Jaoude <eddie@jaoudestudios.com>
2024-07-09 18:26:00 +0100

    feat: display version number

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,9 +9,10 @@ import {
 } from "@headlessui/react";
 import { MagnifyingGlassIcon } from "@heroicons/react/20/solid";
 import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
-
-import classNames from "@/utils/classNames";
 import Link from "next/link";
+
+import config from "@/config/app.json";
+import classNames from "@/utils/classNames";
 
 const navigation = [
   { name: "Home", href: "/", current: true },
@@ -39,13 +40,16 @@ export default function Header({ session, user }) {
     <Disclosure as="header" className="bg-gray-800">
       <div className="mx-auto max-w-7xl px-2 sm:px-4 lg:divide-y lg:divide-gray-700 lg:px-8">
         <div className="relative flex h-16 justify-between">
-          <div className="relative z-10 flex px-2 lg:px-0">
+          <div className="relative z-10 flex px-2 lg:px-0 gap-6">
             <div className="flex flex-shrink-0 items-center">
               <img
                 alt="Your Company"
                 src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
                 className="h-8 w-auto"
               />
+            </div>
+            <div className="flex items-end text-gray-400 text-sm">
+              v{config.version}
             </div>
           </div>
           <div className="relative z-0 flex flex-1 items-center justify-center px-2 sm:absolute sm:inset-0">
