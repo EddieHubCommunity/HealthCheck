6d950760a014cb7e866608beff1dada9d741952f
Eddie Jaoude <eddie@jaoudestudios.com>
2024-07-09 17:31:20 +0100

    fix: font colour for tagline

--- src/components/Tagline.js
+++ src/components/Tagline.js
@@ -7,9 +7,7 @@ export default function Tagline() {
 
   return (
     <div className="border-b border-gray-200 pb-5">
-      <h3 className="text-base font-semibold leading-6 text-gray-900">
-        {tagline.value}
-      </h3>
+      <h3 className="text-base font-semibold leading-6">{tagline.value}</h3>
     </div>
   );
 }
