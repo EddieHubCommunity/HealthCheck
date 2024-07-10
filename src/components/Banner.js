959c63656f7866d2a9054d9eb9f7eea90cfc72bd
Eddie Jaoude <eddie@jaoudestudios.com>
2024-07-10 12:02:16 +0100

    fix: banner text for flagsmith

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -44,7 +44,7 @@ export default function Banner() {
           target="_blank"
           className="flex-none rounded-full bg-gray-900 px-3.5 py-1 text-sm font-semibold text-white shadow-sm hover:bg-gray-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-900"
         >
-          Go their website <span aria-hidden="true">&rarr;</span>
+          Visit their website <span aria-hidden="true">&rarr;</span>
         </a>
       </div>
       <div className="flex flex-1 justify-end">
