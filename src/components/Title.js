a109510739014bca8cac3d7beb35dd3e3f6940b6
Eddie Jaoude <eddie@jaoudestudios.com>
2024-07-18 11:44:34 +0100

    fix: title to have children

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,7 +1,8 @@
-export default function Title({ text }) {
+export default function Title({ text, children }) {
   return (
-    <div className="border-b border-gray-200 py-5">
+    <div className="border-b border-gray-200 py-5 flex flex-wrap items-center justify-between sm:flex-nowrap">
       <h2 className="text-base font-semibold leading-6">{text}</h2>
+      <div className="flex-shrink-0">{children}</div>
     </div>
   );
 }
