ec97170ee220e124a91503cb11b70a17f1c15844
Eddie Jaoude <eddie@jaoudestudios.com>
2024-07-18 11:45:45 +0100

    feat: button

---
+++ src/components/Button.js
@@ -0,0 +1,10 @@
+export default function Button({ text, url }) {
+  return (
+    <a
+      href={url}
+      className="inline-flex items-center gap-x-2 rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+    >
+      {text}
+    </a>
+  );
+}
