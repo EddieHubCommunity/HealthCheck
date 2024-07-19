93482bbc286ce7bc71d2771ee2c89d7f8ad7e14e
Eddie Jaoude <eddie@jaoudestudios.com>
2024-07-19 11:41:32 +0100

    feat: stats have status colours

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,4 +1,15 @@
+import classNames from "@/utils/classNames";
+
 export default function Stats({ data }) {
+  const colours = {
+    success:
+      "text-green-400 bg-green-400/10 ring-green-400/20 border border-green-400",
+    warning:
+      "text-orange-400 bg-orange-400/10 ring-orange-400/30 border border-orange-400",
+    error:
+      "text-rose-500 bg-rose-500/10 ring-rose-500/30 border border-rose-400",
+  };
+
   return (
     <div>
       <h3 className="text-base font-semibold leading-6 text-gray-900">
@@ -8,12 +19,13 @@ export default function Stats({ data }) {
         {data.map((item) => (
           <div
             key={item.name}
-            className="overflow-hidden rounded-lg bg-white px-4 py-5 shadow sm:p-6"
+            className={classNames(
+              "overflow-hidden rounded-lg px-4 py-5 shadow sm:p-6",
+              item.status ? colours[item.status] : "border border-white"
+            )}
           >
-            <dt className="truncate text-sm font-medium text-gray-500">
-              {item.name}
-            </dt>
-            <dd className="mt-1 text-3xl font-semibold tracking-tight text-gray-900">
+            <dt className="truncate text-sm font-medium">{item.name}</dt>
+            <dd className="mt-1 text-3xl font-semibold tracking-tight">
               {item.stat}
             </dd>
           </div>
