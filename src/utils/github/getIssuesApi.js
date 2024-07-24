d12e051094dbdae0a187ff19ecfd8df6314b5370
Eddie Jaoude <eddie@jaoudestudios.com>
2024-07-24 15:44:23 +0100

    feat: get repo issue data from gh

---
+++ src/utils/github/getIssuesApi.js
@@ -0,0 +1,25 @@
+import { Octokit } from "@octokit/rest";
+import extractOwnerRepo from "./extractOwnerRepo";
+
+export default async function getIssuesApi(url, token) {
+  // get owner and repo name from url
+  const { owner, repo } = extractOwnerRepo(url);
+
+  // get data from github api using user's API
+  const octokit = new Octokit({
+    auth: token,
+  });
+  let response;
+  try {
+    response = await octokit.rest.issues.listForRepo({
+      owner,
+      repo,
+      labels: ["good first issue"],
+      assignee: "none",
+    });
+  } catch (e) {
+    console.error(e);
+  }
+
+  return response;
+}
