0cec1ddac94bbd0de608b368b551b173c804e0ac
Amanda <97615019+amandamartin-dev@users.noreply.github.com>
2024-07-27 14:12:38 +0000

    update test

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,18 +1,18 @@
 import { test, expect } from "@playwright/test";
 import { login, logout } from "./setup/auth";
 
-test("Guest user cannot access dashboard", async ({ browser }) => {
+test("Guest user cannot access add repo", async ({ browser }) => {
   // fixture: make sure user is logged out
   const context = await logout(browser);
   const page = await context.newPage();
- // await page.goto("/account/statistics");
- // await expect(page).toHaveURL(/\//);
+  await page.goto("/account/repo/add");
+  await expect(page).toHaveURL(/\//);
 });
 
-test("Logged in user can access dashboard", async ({ browser }) => {
+test("Logged in user can access add repo", async ({ browser }) => {
   // fixture: make sure user is logged in
   const context = await login(browser);
   const page = await context.newPage();
-  //await page.goto("/account/statistics");
- // await expect(page).toHaveURL(/account\/statistics/);
+  await page.goto("/account/repo/add");
+  await expect(page).toHaveURL(/account\/repo\/add/);
 });
