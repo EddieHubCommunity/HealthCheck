237af1652dccd8d8f9d644091e32c9f391011cff
Eddie Jaoude <eddie@jaoudestudios.com>
2024-08-03 10:59:17 +0100

    fix: mswjs missed requests output

--- src/instrumentation.js
+++ src/instrumentation.js
@@ -4,6 +4,23 @@ export async function register() {
     process.env.NODE_ENV === "development"
   ) {
     const { mockServer } = await import("../tests/setup/mocks/node");
-    mockServer.listen();
+
+    mockServer.listen({
+      onUnhandledRequest(request, print) {
+        const url = new URL(request.url);
+        const unmocked = [
+          "googleapis.com",
+          "gstatic.com",
+          "api.flagsmith.com",
+          "https://github.com/mona.png",
+        ];
+        if (unmocked.some((host) => url.hostname.includes(host))) {
+          return;
+        }
+
+        // Print the regular MSW unhandled request warning otherwise.
+        print.warning();
+      },
+    });
   }
 }
