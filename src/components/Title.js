38f1d6143ca53a02f6c3a53cd675a6185a75641e
Eddie Jaoude <eddie@jaoudestudios.com>
2024-08-05 22:48:28 +0100

    fix: title button actions

--- src/components/Title.js
+++ src/components/Title.js
@@ -2,7 +2,7 @@ export default function Title({ text, children }) {
   return (
     <div className="border-b border-gray-200 py-5 flex flex-wrap items-center justify-between sm:flex-nowrap">
       <h2 className="text-base font-semibold leading-6">{text}</h2>
-      <div className="flex-shrink-0">{children}</div>
+      <div className="flex-shrink-0 inline-flex gap-2">{children}</div>
     </div>
   );
 }
