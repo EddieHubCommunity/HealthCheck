45e80ce5310ecedc72db264212dd9b5c09ab5fe5
Eddie Jaoude <eddie@jaoudestudios.com>
2024-08-05 22:47:45 +0100

    fix: button size and secondary color

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,16 @@
-export default function Button({ text, url }) {
+import classNames from "@/utils/classNames";
+
+export default function Button({ text, url, type = "primary" }) {
   return (
     <a
       href={url}
-      className="inline-flex items-center gap-x-2 rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+      className={classNames(
+        "inline-flex items-center gap-x-2 rounded-md px-3.5 py-2.5 text-sm font-semibold shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2",
+        type === "primary" &&
+          "text-white bg-indigo-600 hover:bg-indigo-500 focus-visible:outline-indigo-600",
+        type === "secondary" &&
+          "text-white bg-violet-600 hover:bg-violet-500 focus-visible:outline-violet-600",
+      )}
     >
       {text}
     </a>
