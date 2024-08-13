8377276bc2f855a1fdeb952d76fdff8a032d3ba3
Eddie Jaoude <eddie@jaoudestudios.com>
2024-08-13 21:10:42 +0100

    fix: banner text

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,4 +1,5 @@
 import { XMarkIcon } from "@heroicons/react/20/solid";
+import Link from "next/link";
 
 export default function Banner() {
   return (
@@ -29,7 +30,7 @@ export default function Banner() {
       </div>
       <div className="flex flex-wrap items-center gap-x-4 gap-y-2">
         <p className="text-sm leading-6 text-gray-900">
-          <strong className="font-semibold">A big thank you to</strong>
+          <strong className="font-semibold">Attention</strong>
           <svg
             viewBox="0 0 2 2"
             aria-hidden="true"
@@ -37,15 +38,15 @@ export default function Banner() {
           >
             <circle r={1} cx={1} cy={1} />
           </svg>
-          Flagsmith for sponsoring this project
+          A big thank you to Flagsmith for sponsoring this project
         </p>
-        <a
+        <Link
           href="https://www.flagsmith.com"
           target="_blank"
           className="flex-none rounded-full bg-gray-900 px-3.5 py-1 text-sm font-semibold text-white shadow-sm hover:bg-gray-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-900"
         >
           Visit their website <span aria-hidden="true">&rarr;</span>
-        </a>
+        </Link>
       </div>
       <div className="flex flex-1 justify-end">
         {/* <button
