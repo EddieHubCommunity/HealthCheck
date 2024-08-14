51bbd276470627777f2d4d8c4e71009852e2f08a
Eddie Jaoude <eddie@jaoudestudios.com>
2024-08-14 22:03:43 +0100

    fix: repo with no description

--- src/utils/checks/description.js
+++ src/utils/checks/description.js
@@ -21,19 +21,19 @@ export default function description(repo) {
       "It is important to write a concise description about your repo.";
   }
 
-  if (repo.description.length < min) {
+  if (repo.description?.length < min) {
     response.status = "warning";
     response.description = "Your description may be too short.";
     response.extra = "Try to include more information.";
   }
 
-  if (repo.description.length > max) {
+  if (repo.description?.length > max) {
     response.status = "warning";
     response.description = "Your description may be too long.";
     response.extra = "Try reducing the length of your description.";
   }
 
-  if (repo.homepage?.length > 0 && repo.description.includes(repo.homepage)) {
+  if (repo.homepage?.length > 0 && repo.description?.includes(repo.homepage)) {
     response.status = "warning";
     response.description = "Your description contains a duplicate of the url.";
     response.extra = "You can remove the url from the description.";
