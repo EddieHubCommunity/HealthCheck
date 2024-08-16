5001e08275bccd96026a081f05abacf2cb948772
Bridget Amana <amanabridget03@gmail.com>
2024-08-16 07:51:15 +0100

    fix: footer margin-top #159 (#169)
    * fix: add margin-top to footer
    
    * remove mt-auto

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -32,7 +32,7 @@ const navigation = [
 
 export default function Footer() {
   return (
-    <footer className="bg-gray-800 mt-auto">
+    <footer className="bg-gray-800 mt-5">
       <div className="mx-auto max-w-7xl px-6 py-12 md:flex md:items-center md:justify-between lg:px-8">
         <div className="flex justify-center space-x-6 md:order-2">
           {navigation.map((item) => (
