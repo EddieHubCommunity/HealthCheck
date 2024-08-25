85f9eafa35548813d7f73c45cd60bd7cbd9ca59d
Eddie Jaoude <eddie@jaoudestudios.com>
2024-08-25 11:57:01 +0100

    Update src/components/forms/Input.js

--- src/components/forms/Input.js
+++ src/components/forms/Input.js
@@ -62,7 +62,7 @@ export default function Input({
   }
 
   return (
-    <div className={classNames("col-span-full ", className)}>
+    <div className={classNames("col-span-full", className)}>
       <label
         htmlFor={id}
         className={classNames(
