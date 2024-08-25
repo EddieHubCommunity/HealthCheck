25e9733e25c821a2332548c0f0370ff4cdc034d9
Adam Basha <110662505+Bashamega@users.noreply.github.com>
2024-08-25 14:11:25 +0300

    Update src/app/account/repo/checks/[id]/form.js
    Co-authored-by: Eddie Jaoude <eddie@jaoudestudios.com>

--- src/app/account/repo/checks/[id]/form.js
+++ src/app/account/repo/checks/[id]/form.js
@@ -52,7 +52,7 @@ export function FormBadge({ src }) {
             labelClassName="hidden"
           />
         </div>
-        {copy && <p className=" text-green-400 mt-2">Copied!</p>}
+        {copy && <p className="text-green-400 mt-2">Copied!</p>}
         {/* <button
           type="button"
           className="relative -ml-px inline-flex items-center gap-x-1.5 rounded-r-md px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
