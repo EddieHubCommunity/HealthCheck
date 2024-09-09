2a3798b5cbc440300760a886cd0256a00b73637d
Adam Basha <110662505+Bashamega@users.noreply.github.com>
2024-09-09 14:15:05 +0300

    feat: copy badge feature (#175)
    * feat: added copy feature
    
    * Switched to asyc
    
    * Removed the console log
    
    * Format fix
    
    * Updated input
    
    * format
    
    * Uses classnames
    
    * Update src/components/forms/Input.js
    
    * Update src/app/account/repo/checks/[id]/form.js
    
    Co-authored-by: Eddie Jaoude <eddie@jaoudestudios.com>
    
    * Update src/app/account/repo/checks/[id]/form.js
    
    Co-authored-by: Eddie Jaoude <eddie@jaoudestudios.com>
    
    * patch: class names
    
    * patch: format
    
    * Update src/app/account/repo/checks/[id]/form.js
    
    Co-authored-by: Eddie Jaoude <eddie@jaoudestudios.com>
    
    * Update src/app/account/repo/checks/[id]/form.js
    
    ---------
    
    Co-authored-by: Eddie Jaoude <eddie@jaoudestudios.com>

--- src/app/account/repo/checks/[id]/form.js
+++ src/app/account/repo/checks/[id]/form.js
@@ -3,11 +3,14 @@
 import {
   DocumentDuplicateIcon,
   CheckBadgeIcon,
+  ClipboardDocumentCheckIcon,
 } from "@heroicons/react/20/solid";
+import { useState } from "react";
 
 import { performChecks } from "./action";
 import Input from "@/components/forms/Input";
 import { SubmitButton } from "@/components/forms/SubmitButton";
+import classNames from "@/utils/classNames";
 
 export default function Form({ id }) {
   return (
@@ -19,31 +22,41 @@ export default function Form({ id }) {
 }
 
 export function FormBadge({ src }) {
+  const [copy, setCopy] = useState(false);
+  const copyHandle = async () => {
+    const url = `![HealthCheck](${src})`;
+    await navigator.clipboard.writeText(url);
+    setCopy(true);
+  };
   return (
     <div>
-      <label
-        htmlFor="email"
-        className="block text-sm font-medium leading-6 text-white"
-      >
+      <label>
         Add badge to your Repo&lsquo;s README to show the latest check status
       </label>
-      <div className="mt-2 flex rounded-md shadow-sm">
+      <div className="mt-2  rounded-md shadow-sm">
         <div className="relative flex flex-grow items-stretch focus-within:z-10">
-          <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-            <CheckBadgeIcon
+          <div className="absolute inset-y-0 left-0 flex items-center pl-3 cursor-pointer">
+            <ClipboardDocumentCheckIcon
               aria-hidden="true"
-              className="h-5 w-5 text-gray-400"
+              className={classNames(
+                "h-5 w-5",
+                copy ? "text-green-400 " : "text-gray-500 ",
+              )}
+              onClick={copyHandle}
             />
           </div>
-          <input
+          <Input
             readOnly={true}
             id="badge"
             name="badge"
             type="text"
             value={`![HealthCheck](${src})`}
-            className="block w-full rounded-md border-0 py-1.5 pl-10 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            className="w-full -z-30"
+            inputClassName="pl-10 py-1.5"
+            labelClassName="hidden"
           />
         </div>
+        {copy && <p className="text-green-400 mt-2">Copied!</p>}
         {/* <button
           type="button"
           className="relative -ml-px inline-flex items-center gap-x-1.5 rounded-r-md px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
--- src/components/forms/Input.js
+++ src/components/forms/Input.js
@@ -12,6 +12,9 @@ export default function Input({
   prepend,
   disabled = false,
   type = "text",
+  className,
+  inputClassName,
+  labelClassName,
 }) {
   const { pending } = useFormStatus();
 
@@ -37,6 +40,7 @@ export default function Input({
         prepend
           ? "flex-1 bg-transparent pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0"
           : "w-full rounded-md py-1.5 pr-10 ring-1 ring-inset focus:ring-2 focus:ring-inset",
+        inputClassName,
       )}
       aria-invalid="true"
       aria-describedby={`${id}-error`}
@@ -58,8 +62,14 @@ export default function Input({
   }
 
   return (
-    <div className="col-span-full">
-      <label htmlFor={id} className="block text-sm font-medium leading-6">
+    <div className={classNames("col-span-full", className)}>
+      <label
+        htmlFor={id}
+        className={classNames(
+          "block text-sm font-medium leading-6",
+          labelClassName,
+        )}
+      >
         {text}
       </label>
       <div className="relative mt-2 rounded-md shadow-sm">
