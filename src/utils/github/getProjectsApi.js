fd742b728c3b20f35077b3dce5e97eae7714fc0b
Saptarshi Sarkar <saptarshi.programmer@gmail.com>
2024-09-24 21:06:06 +0530

    feat: Moved GitHub Projects API usage from REST to GraphQL

--- src/utils/github/getProjectsApi.js
+++ src/utils/github/getProjectsApi.js
@@ -1,4 +1,4 @@
-import { Octokit } from "@octokit/rest";
+import { graphql } from "@octokit/graphql";
 import extractOwnerRepo from "./extractOwnerRepo";
 
 export default async function getProjectsApi(url, token) {
@@ -6,16 +6,38 @@ export default async function getProjectsApi(url, token) {
   const { owner, repo } = extractOwnerRepo(url);
 
   // get data from github api using user's API
-  const octokit = new Octokit({
-    auth: token,
+  const octokit = graphql.defaults({
+    headers: {
+      authorization: `token ${token}`,
+    },
   });
   let response;
   try {
-    response = await octokit.rest.projects.listForRepo({
-      owner,
-      repo,
-      state: "open",
-    });
+    response = await octokit(
+      `
+      query($owner: String!, $repo: String!) {
+        repository(owner: $owner, name: $repo) {
+          projectsV2(first: 10) {
+            nodes {
+              id
+              title
+              closed
+            }
+          }
+        }
+      }
+      `,
+      {
+        owner,
+        repo,
+      },
+    );
+    console.log(response);
+    console.error(response);
+    response = {
+      status: 200,
+      data: response.repository.projectsV2.nodes,
+    };
   } catch (e) {
     console.error(e);
     response = {
@@ -23,6 +45,7 @@ export default async function getProjectsApi(url, token) {
       data: [],
     };
   }
+  console.log(response);
 
   return response;
 }
