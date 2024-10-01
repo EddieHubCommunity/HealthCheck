3db9e0fa8353477f16809c5b582f157199cfd0d7
Saptarshi Sarkar <saptarshi.programmer@gmail.com>
2024-10-01 15:29:29 +0530

    feat: gh projects v2 to GraphQL (#267)
    * feat: Moved GitHub Projects API usage from REST to GraphQL
    
    * Update src/utils/github/getProjectsApi.js
    
    * Update src/utils/github/getProjectsApi.js
    
    ---------
    
    Co-authored-by: Eddie Jaoude <eddie@jaoudestudios.com>

--- src/utils/checks/projects.js
+++ src/utils/checks/projects.js
@@ -11,14 +11,18 @@ export default function projects(repo, projectsData) {
     response.extra = "No action required.";
   }
 
-  if (repo.has_projects && projectsData.length > 0) {
+  const filteredProjectsData = projectsData.filter(
+    (project) => !project.closed,
+  ); // filter out closed projects
+
+  if (repo.has_projects && filteredProjectsData.length > 0) {
     response.status = "success";
     response.description =
       "You have project boards enabled and it is being used.";
     response.extra = "No action required.";
   }
 
-  if (repo.has_projects && projectsData.length === 0) {
+  if (repo.has_projects && filteredProjectsData.length === 0) {
     response.status = "error";
     response.description =
       "You have project boards enabled but it is not being used.";
